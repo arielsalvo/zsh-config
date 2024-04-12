404221748ac3b77af1062d858dc707928c86f82f
Ariel Salvo <ariel.salvo@gmail.com>
2024-04-12 18:33:59 -0300

    Convert zi_helper aliases to functions

--- aliases.d/zi_helpers.zsh
+++
@@ -1,4 +0,0 @@
-# Show all entries in the ZI array
-
-alias zi_config='for key in $(echo ${(k)ZI} | sed "s/ /\n/g" | sort); do echo "$key -> ${ZI[$key]}"; done'
-alias zi_featureflags='for key in $(echo ${(k)ZI_FEATURES} | sed "s/ /\n/g" | sort); do echo "$key -> ${ZI_FEATURES[$key]}"; done'
--- functions.d/zi_helpers.zsh
+++ functions.d/zi_helpers.zsh
@@ -1,3 +1,17 @@
+# Show all entries in the ZI array
+function zi_config() {
+  for key in $(echo ${(k)ZI} | sed "s/ /\n/g" | sort); do
+    echo "$key -> ${ZI[$key]}";
+  done
+}
+
+# List all Feature flags
+function zi_featureflags() {
+  for key in $(echo ${(k)ZI_FEATURES} | sed "s/ /\n/g" | sort); do
+    echo "$key -> ${ZI_FEATURES[$key]}";
+  done
+}
+
 # Test if a Feature flag is set and true
 function zi_featureflag_enabled() {
   [[ $# < 1 ]] && return 1
