790e1eb35a4413e560dcf8fc7b92ee12e9c1d7bb
Ariel Salvo <ariel.salvo@gmail.com>
2024-04-16 15:54:37 -0300

    Initialize `conda`

--- zshrc.d/20-apps-05-python.zsh
+++ zshrc.d/20-apps-05-python.zsh
@@ -2,25 +2,22 @@ zi_featureflag_enabled "PYTHON" || return;
 
 zi pack"bgn" for pyenv
 
-# if type brew &>/dev/null
-# then
-#     # conda init -q "$(basename "${SHELL}")"
-
-#     # >>> conda initialize >>>
-#     # !! Contents within this block are managed by 'conda init' !!
-#     __conda_setup="$('/usr/local/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-#     if [ $? -eq 0 ]; then
-#         eval "$__conda_setup"
-#     else
-#         if [ -f "/usr/local/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
-#             . "/usr/local/Caskroom/miniconda/base/etc/profile.d/conda.sh"
-#         else
-#             export PATH="/usr/local/Caskroom/miniconda/base/bin:$PATH"
-#         fi
-#     fi
-#     unset __conda_setup
-#     # <<< conda initialize <<<
-# fi
+if command -v "conda" &> /dev/null; then
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$(pyenv prefix)/etc/profile.d/conda.sh" ]; then
+        . "$(pyenv prefix)/etc/profile.d/conda.sh"
+    else
+        export PATH="$(pyenv prefix)/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+fi
 
 # eval "$(register-python-argcomplete pipx)"
 
