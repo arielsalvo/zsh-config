d1d6d10e1c7310a83de55b2a7b8a8000626143ea
Ariel Salvo <ariel.salvo@gmail.com>
2024-04-16 09:54:16 -0300

    Better direnv installation

--- zshrc.d/10-pkg-02-asdf.zsh
+++ zshrc.d/10-pkg-02-asdf.zsh
@@ -1,12 +1,14 @@
 _zini_asdf_install() {
     # path export temporary required so the asdf script finds itself
     PATH="${PWD}/bin:$PATH"
-    asdf plugin add direnv
-    asdf install direnv latest
-    asdf global direnv latest
-    # see https://github.com/asdf-community/asdf-direnv#setup
-    asdf exec direnv hook zsh > asdf_direnv_hook.zsh
-    echo 'direnv() { asdf exec direnv "$@"; }' >> asdf_direnv_hook.zsh
+    echo "" > asdf_hooks.zsh
+
+    # Example install of direnv:
+    # asdf plugin add direnv
+    # asdf install direnv latest
+    # asdf global direnv latest
+    # asdf exec direnv hook zsh >> asdf_hooks.zsh # see https://github.com/asdf-community/asdf-direnv#setup
+    # echo 'direnv() { asdf exec direnv "$@"; }' >> asdf_hooks.zsh
 }
 
 zi wait:'0b' \
@@ -15,7 +17,8 @@ zi wait:'0b' \
     sbin:'bin/asdf' \
     atinit:'export ASDF_DIR="$PWD"' \
     atclone:'_zini_asdf_install' \
+    atpull'%atclone' \
     src:'asdf.sh' \
-    multisrc:'asdf_direnv_hook.zsh' \
+    multisrc:'asdf_hooks.zsh' \
     for \
     depth:1 @asdf-vm/asdf
---
+++ zshrc.d/20-apps-01-direnv.zsh
@@ -0,0 +1,11 @@
+# Install direnv from release and hook it into the shell
+zi for \
+  from"gh-r" \
+  as"program" \
+  mv"direnv* -> direnv" \
+  atclone'./direnv hook zsh > zhook.zsh' \
+  atpull'%atclone' \
+  pick"direnv" \
+  src="zhook.zsh" \
+  wait'0' lucid \
+  load direnv/direnv
