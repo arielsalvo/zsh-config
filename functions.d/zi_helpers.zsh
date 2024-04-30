f633ecd5bcb4d6ae660e9c588b8401d160653f0b
Ariel Salvo <ariel.salvo@gmail.com>
2024-04-30 18:05:42 -0300

    Move ZSHCONF_FEATUREFLAG* from helpers to the featureflags file

--- functions.d/zi_helpers.zsh
+++ functions.d/zi_helpers.zsh
@@ -4,22 +4,3 @@ function zi_config() {
     echo "$key -> ${ZI[$key]}";
   done
 }
-
-# List all Feature flags
-function zshconf_featureflags() {
-  for key in $(echo ${(k)ZSHCONF_FEATURES} | sed "s/ /\n/g" | sort); do
-    echo "$key -> ${ZSHCONF_FEATURES[$key]}";
-  done
-}
-
-# Test if a Feature flag is set and true
-function zshconf_featureflag_enabled() {
-  [[ $# < 1 ]] && return 1
-
-  # Check if the feature is enabled
-  if (( ${+ZSHCONF_FEATURES[$1]} )); then
-    return 0
-  else
-    return 1
-  fi
-}
--- zshrc.d/00-zi-02-featureflags.zsh
+++ zshrc.d/00-zi-02-featureflags.zsh
@@ -13,3 +13,22 @@ for _featurefile in ${ZDOTDIR}/.featureflags/*(.N); do
   typeset -gx ZSHCONF_FEATURES["${_featurefile:t}"]=$(cat ${_featurefile})
 done
 unset _featurefile
+
+# List all Feature flags
+function zshconf_featureflags() {
+  for key in $(echo ${(k)ZSHCONF_FEATURES} | sed "s/ /\n/g" | sort); do
+    echo "$key -> ${ZSHCONF_FEATURES[$key]}";
+  done
+}
+
+# Test if a Feature flag is set and true
+function zshconf_featureflag_enabled() {
+  [[ $# < 1 ]] && return 1
+
+  # Check if the feature is enabled
+  if (( ${+ZSHCONF_FEATURES[$1]} )); then
+    return 0
+  else
+    return 1
+  fi
+}
