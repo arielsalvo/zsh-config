fa0bada01930ae82f8cada823a09b9e3c47ed231
Ariel Salvo <ariel.salvo@gmail.com>
2024-05-07 11:33:28 -0300

    Add zsh-abbr to complement aliases

--- zshrc.d/03-completions.zsh
+++ zshrc.d/03-completions.zsh
@@ -1,3 +1,9 @@
 
 # Add support for completions, suggestions and syntax highlighting (meta-plugin)
 zi load @zsh-users+fast
+
+# Use the zsh-abbr to complement aliases/suggestions/completions
+# (https://zsh-abbr.olets.dev/)
+zi for \
+  atload'typeset -gx ABBR_SET_EXPANSION_CURSOR=1' \
+  load olets/zsh-abbr
