04514ea15b1c6d9cd16c320462da820ea8b6b5e5
Ariel Salvo <ariel.salvo@gmail.com>
2024-05-08 16:58:32 -0300

    Add Packer completions

--- zshrc.d/20-apps-15-hashicorp.zsh
+++ zshrc.d/20-apps-15-hashicorp.zsh
@@ -1,8 +1,16 @@
 # Load completion for terraform if installed
-
 zi for \
   wait'!0b' \
   has'terraform' \
   lucid \
   load \
   noahingh/terraform-oh-my-zsh-plugin
+
+
+# Load completion for packer if installed
+zi for \
+  wait'!0b' \
+  has'packer' \
+  lucid \
+  load \
+  BreakingPitt/zsh-packer.git
