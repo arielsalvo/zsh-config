59f4820dba25f9f152fcebdb0109da1a1d3ee067
Ariel Salvo <ariel.salvo@gmail.com>
2024-05-09 10:34:36 -0300

    packer and terraform detection not working, loading regardless

--- zshrc.d/20-apps-15-hashicorp.zsh
+++ zshrc.d/20-apps-15-hashicorp.zsh
@@ -1,16 +1,16 @@
-# Load completion for terraform if installed
+## FIXME: Detect if terraform and packer are installed to load completions
+
+# Load completion for terraform
 zi for \
-  wait'!0b' \
-  has'terraform' \
+  wait'0c' \
   lucid \
   load \
   noahingh/terraform-oh-my-zsh-plugin
 
 
-# Load completion for packer if installed
+# Load completion for packer
 zi for \
-  wait'!0b' \
-  has'packer' \
+  wait'0c' \
   lucid \
   load \
   BreakingPitt/zsh-packer
