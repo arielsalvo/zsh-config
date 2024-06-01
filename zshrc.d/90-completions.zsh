137aac98dc709524c6c085fcb708510fb494d7c7
Ariel Salvo <ariel.salvo@gmail.com>
2024-06-01 10:34:27 -0300

    Make zsh-users load last because of syntax highlighting

--- zshrc.d/90-completions.zsh
+++ zshrc.d/90-completions.zsh
@@ -1,9 +1,9 @@
-
-# Add support for completions, suggestions and syntax highlighting (meta-plugin)
-zinit load @zsh-users+fast
-
 # Use the zsh-abbr to complement aliases/suggestions/completions
 # (https://zsh-abbr.olets.dev/)
 zinit for \
   atload'typeset -gx ABBR_SET_EXPANSION_CURSOR=1' \
   load olets/zsh-abbr
+
+# Add support for completions, suggestions and syntax highlighting (meta-plugin)
+# This should be the last plugin loaded
+zinit load @zsh-users+fast
