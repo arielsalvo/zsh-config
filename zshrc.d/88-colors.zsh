c08aaa700bdcda73f919699e16b6a3f776ceae3b
Ariel Salvo <ariel.salvo@gmail.com>
2024-06-07 13:51:35 -0300

    Switch to the official dircolors plugin

--- zshrc.d/88-colors.zsh
+++ zshrc.d/88-colors.zsh
@@ -1,4 +1,4 @@
 # Apply Neutral color scheme
 # Make sure your terminal has an appropriate colorscheme
 # (e.g. https://github.com/zpm-zsh/terminal-colorscheme-neutral)
-zinit load zpm-zsh/theme-neutral
+zinit pack for dircolors-material
