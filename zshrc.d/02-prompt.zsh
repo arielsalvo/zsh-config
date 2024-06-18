a25462d73213888b7338034a00589b2355748e99
Ariel Salvo <ariel.salvo@gmail.com>
2024-06-18 14:16:53 -0300

    Improve prompt plugin loading

--- zshrc.d/02-prompt.zsh
+++ zshrc.d/02-prompt.zsh
@@ -4,21 +4,24 @@ case ${(U)ZSHCONF_FEATURES[DEFAULT_PROMPT]:-"P10K"} in
   # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh
   [[ -f ${HOME}/.p10k.zsh ]] || ln -s ${ZDOTDIR}/files.d/.p10k.zsh ${HOME}/.p10k.zsh
   zinit for \
-    depth"1" \
+    depth'1' \
     atinit'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' \
-    atload"!source ${HOME}/.p10k.zsh" \
+    atload'!source ${HOME}/.p10k.zsh' \
     nocd \
-    light-mode romkatv/powerlevel10k
+      light-mode romkatv/powerlevel10k
 ;;
 "STARSHIP")
   # Load starship theme
-  # line 1: `starship` binary as command, from github release
-  # line 2: starship setup at clone(create init.zsh, completion)
-  # line 3: pull behavior same as clone, source init.zsh
-  zinit ice as"command" from"gh-r" \
-            atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
-            atpull"%atclone" src"init.zsh"
-  zinit light starship/starship
+  # * `starship` binary as command, from github release
+  # * starship setup at clone(create init.zsh, completion)
+  # * pull behavior same as clone, source init.zsh
+  zinit for \
+    as'command' \
+    from'gh-r' \
+    atclone'./starship init zsh > init.zsh; ./starship completions zsh > _starship' \
+    atpull'%atclone' \
+    src'init.zsh' \
+      light-mode starship/starship
 ;;
 "DEFAULT"|*)
   # Use Zsh native prompt
