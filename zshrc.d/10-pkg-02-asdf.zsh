b659268e991f696adb5d5377ffaa4aebe50b54e3
Ariel Salvo <ariel.salvo@gmail.com>
2025-01-30 18:00:46 -0300

    Update ASDF to v0.16+ (go rewrite)

--- zshrc.d/10-pkg-02-asdf.zsh
+++ zshrc.d/10-pkg-02-asdf.zsh
@@ -1,8 +1,10 @@
 # Install and initialize ASDF
-zinit for \
-    sbin:'bin/asdf' \
-    atinit:'export ASDF_DIR="$PWD"; [[ -f ${HOME}/.asdfrc ]] || ln -s ${ZDOTDIR}/files.d/.asdfrc ${HOME}/.asdfrc;' \
-    atpull'%atclone' \
-    src:'asdf.sh' \
-    lucid \
-    depth:1 @asdf-vm/asdf
+zinit \
+  wait \
+  from"gh-r" \
+  as"program" \
+  atinit:'export ASDF_DIR="$PWD"; [[ -f ${HOME}/.asdfrc ]] || ln -s ${ZDOTDIR}/files.d/.asdfrc ${HOME}/.asdfrc;' \
+  atpull'%atclone' \
+  pick"direnv" \
+  lucid \
+  for @asdf-vm/asdf
