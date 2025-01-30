eec0800aac9bbeba6869cb893757a442ceafe816
Ariel Salvo <ariel.salvo@gmail.com>
2025-01-30 18:48:20 -0300

    Add ASDF data dir shims to path

--- zshrc.d/10-pkg-02-asdf.zsh
+++ zshrc.d/10-pkg-02-asdf.zsh
@@ -3,8 +3,14 @@ zinit \
   wait \
   from"gh-r" \
   as"program" \
-  atinit:'export ASDF_DIR="$PWD"; [[ -f ${HOME}/.asdfrc ]] || ln -s ${ZDOTDIR}/files.d/.asdfrc ${HOME}/.asdfrc;' \
+  atinit:'export ASDF_DIR="$PWD"; export ASDF_DATA_DIR="$HOME/.asdf"; [[ -f ${HOME}/.asdfrc ]] || ln -s ${ZDOTDIR}/files.d/.asdfrc ${HOME}/.asdfrc; ./asdf completion zsh > "_asdf"' \
   atpull'%atclone' \
   pick"direnv" \
   lucid \
   for @asdf-vm/asdf
+
+# Add ASDF shim to path
+path=(
+  ${ASDF_DATA_DIR:-$HOME/.asdf}/shims(N)
+  $path
+)
