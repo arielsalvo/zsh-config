ed31ebbf5803060107ae2ecde33b992a8d4723e7
Ariel Salvo <ariel.salvo@gmail.com>
2025-02-03 17:45:06 -0300

    Improve homebrew integration

--- zshrc.d/10-pkg-01-homebrew.zsh
+++ zshrc.d/10-pkg-01-homebrew.zsh
@@ -1,8 +1,16 @@
-# MacOS
-if [[ "$OSTYPE" == darwin* ]]; then
-  # Do not send Homebrew analytics
-  export HOMEBREW_NO_ANALYTICS=1
+# Is HomeBrew installed and in the path?
+if command -v brew &> /dev/null; then
 
-  # Do not print any hints about changing Homebrew's behaviour with environment variables
-  export HOMEBREW_NO_ENV_HINTS=1
+  # MacOS
+  if [[ "$OSTYPE" == darwin* ]]; then
+    # Execute Homebrew shell integrations
+    eval "$(/usr/local/bin/brew shellenv)"
+
+    # Do not send Homebrew analytics
+    export HOMEBREW_NO_ANALYTICS=1
+
+    # Do not print any hints about changing Homebrew's behaviour with environment variables
+    export HOMEBREW_NO_ENV_HINTS=1
+  fi
+
 fi
